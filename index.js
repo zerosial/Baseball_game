cac4ed624710de895758a8a02f620e4a4f9950fe
zerosial <zerosial@gmail.com>
2022-03-06 00:17:16 +0900

    3/6

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("hello, vanilla.");
+const Gamestart = document.getElementById("gamestart");     // 게임 스타트 버튼
+
+Gamestart.addEventListener('click',function () {            // 클릭시 렌덤한 숫자 생성
+let answer = Math.floor(Math.random() * 1000);
+});
+

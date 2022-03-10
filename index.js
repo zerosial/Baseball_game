1bbd396926390137faf65fa40a4003814a1a50c2
zerosial <zerosial@gmail.com>
2022-03-10 21:49:04 +0900

    3/10 완료

--- index.js
+++ index.js
@@ -1,6 +1,3 @@
-const Gamestart = document.getElementById("gamestart");     // 게임 스타트 버튼
-
-Gamestart.addEventListener('click',function () {            // 클릭시 렌덤한 숫자 생성
-let answer = Math.floor(Math.random() * 1000);
-});
-
+function playGame() {                                        // gamestart 클릭 시 playgame 화면으로 이동
+    window.location.href = 'playgame.html'
+}
